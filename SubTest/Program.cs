1be378eb37241b5c023d73ffd6159bea8e0e1a87
Koen D'Heedene <koene.dheedene@gmail.com>
2023-11-05 19:23:10 +0100

    start using myhive

--- SubTest/Program.cs
+++ SubTest/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace SubTest
+﻿using MyLib;
+namespace SubTest
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Hive hive = new Hive();
+            hive.GivemeaBool();
         }
     }
 }
